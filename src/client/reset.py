04b57d2a5271d78c39b32a8dc48db0c867dae292
kotime1 <thenewbryan@gmail.com>
2025-04-24 22:19:20 -0400

    reset.py update

--- src/client/reset.py
+++ src/client/reset.py
@@ -1,7 +1,19 @@
 import os
+import shutil
 
-os.remove("../server/output")
-os.remove("./encrypted_predictions.pkl")
-os.remove("./params")
-os.remove("./data/encrypted_user_data.pkl")
-os.remove("./data/predictions.csv")
+def safe_remove(path):
+    if os.path.isfile(path):
+        os.remove(path)
+        print(f"Removed file: {path}")
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+        print(f"Removed directory: {path}")
+
+# Client-side cleanup
+safe_remove("./data/encrypted_user_data.pkl")
+safe_remove("./data/predictions.csv")
+safe_remove("./params")
+safe_remove("./encrypted_predictions.pkl")
+
+# Server-side cleanup
+safe_remove("../server/output")
