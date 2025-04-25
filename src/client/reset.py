441e68cb90eb005415a22036b8fdabb735474781
kotime1 <thenewbryan@gmail.com>
2025-04-24 22:15:40 -0400

    script to reset demo

---
+++ src/client/reset.py
@@ -0,0 +1,7 @@
+import os
+
+os.remove("../server/output")
+os.remove("./encrypted_predictions.pkl")
+os.remove("./params")
+os.remove("./data/encrypted_user_data.pkl")
+os.remove("./data/predictions.csv")
